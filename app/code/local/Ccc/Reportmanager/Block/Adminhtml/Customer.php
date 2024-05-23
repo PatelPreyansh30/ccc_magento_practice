ab389ecb4602a4f15f6b64d7ffe4a9e86dda0ba6
Preyansh Patel <patelpreyansh3011@gmail.com>
2024-05-23 13:21:41 +0530

    system config for show btn

--- app/code/local/Ccc/Reportmanager/Block/Adminhtml/Customer.php
+++ app/code/local/Ccc/Reportmanager/Block/Adminhtml/Customer.php
@@ -6,15 +6,17 @@ class Ccc_Reportmanager_Block_Adminhtml_Customer extends Mage_Adminhtml_Block_Wi
         $this->_controller = 'customer';
         $this->_headerText = Mage::helper('customer')->__('Manage Customers');
         $this->_addButtonLabel = Mage::helper('customer')->__('Add New Customer');
-        $this->_addButton(
-            'save_report',
-            array(
-                'label' => Mage::helper('catalog')->__('Save Report'),
-                'onclick' => "customerGridJsObject.saveReport()",
-                'class' => 'save-report',
-                'id' => 'save-report-btn',
-            )
-        );
+        if (Mage::getStoreConfig('ccc_reportmanager/options/reportmanager')) {
+            $this->_addButton(
+                'save_report',
+                array(
+                    'label' => Mage::helper('catalog')->__('Save Report'),
+                    'onclick' => "customerGridJsObject.saveReport()",
+                    'class' => 'save-report',
+                    'id' => 'save-report-btn',
+                )
+            );
+        }
         parent::__construct();
     }
 
--- app/code/local/Ccc/Reportmanager/Block/Adminhtml/Product.php
+++ app/code/local/Ccc/Reportmanager/Block/Adminhtml/Product.php
@@ -16,15 +16,17 @@ class Ccc_Reportmanager_Block_Adminhtml_Product extends Mage_Adminhtml_Block_Wid
                 'class' => 'add'
             )
         );
-        $this->_addButton(
-            'save_report',
-            array(
-                'label' => Mage::helper('catalog')->__('Save Report'),
-                'onclick' => "productGridJsObject.saveReport()",
-                'class' => 'save-report',
-                'id' => 'save-report-btn',
-            )
-        );
+        if (Mage::getStoreConfig('ccc_reportmanager/options/reportmanager')) {
+            $this->_addButton(
+                'save_report',
+                array(
+                    'label' => Mage::helper('catalog')->__('Save Report'),
+                    'onclick' => "productGridJsObject.saveReport()",
+                    'class' => 'save-report',
+                    'id' => 'save-report-btn',
+                )
+            );
+        }
 
         $this->setChild('grid', $this->getLayout()->createBlock('adminhtml/catalog_product_grid', 'product.grid'));
         return parent::_prepareLayout();
